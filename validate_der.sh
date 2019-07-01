a686cedd3c3b21f3fe308b3c9c8250fe559c99d5
MarvinLvn <marvin.lavechin@ensimag.grenoble-inp.fr>
2019-07-01 14:43:32 -0400

    Add script for validating with DER

---
+++ validate_der.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+#!/usr/bin/env bash
+#$ -j y -o /home/lmarvin/BabyTrain_multilabel/validate_log.txt
+#$ -e /home/lmarvin/BabyTrain_multilabel/validate_err.txt
+#$ -M marvin.lavechin@ensimag.grenoble-inp.fr
+#$ -l mem_free=10G
+#$ -l ram_free=10G
+#$ -l gpu=1
+#$ -l "hostname=b1[12345678]*|c*"
+#$ -cwd
+
+if [ $# -le 2 ]; then
+    echo "Usage :"
+    echo "./validate.sh <CLASS> <experiment_dir> <protocol> <OPTIONAL protocol_train> <OPTION precision>"
+    echo "<CLASS> needs to be in [KCHI, CHI, FEM, MAL, speech]"
+    echo "<protocol> is the protocol on which the model will be validating"
+    echo "<protocol_train> is the name of the protocol on which the model was trained (if not specified, assuming it's the same one as <protocol>)"
+    echo "<precision> specifies the fixed precision for validation. If not specified, assumes 0.8"
+    echo "Example :"
+    echo "export EXPERIMENT_DIR=babytrain/multilabel"
+    echo "sbatch validate.sh speech ${EXPERIMENT_DIR} BabyTrain.SpeakerRole.JSALT X.SpeakerRole.JSALT 0.5"
+    exit
+fi
+
+
+CLASS=$1
+experiment_dir=$2
+protocol=$3
+protocol_train=$4
+
+# if no protocol_train given, assume the validation protocol is the same as the one used for training
+if [[ ! $protocol_train ]]; then
+    echo "assuming you're validating on same protocol as train..."
+    protocol_train=$protocol;
+fi
+
+if [[ ! $CLASS =~ ^(KCHI|CHI|FEM|MAL|SPEECH)$ ]]; then
+    echo "The first parameter must belong to [KCHI,CHI,FEM,MAL,SPEECH]."
+    exit
+fi
+
+#echo "Began at $(date)"
+#export CUDA_VISIBLE_DEVICES=`free-gpu`
+#echo "Found GPU : $CUDA_VISIBLE_DEVICES"
+
+source activate pyannote
+export EXPERIMENT_DIR=$experiment_dir
+export TRAIN_DIR=${EXPERIMENT_DIR}/train/${protocol_train}.train
+
+pyannote-multilabel validate --every=1 $CLASS ${TRAIN_DIR} $protocol --use_der --batch 16
