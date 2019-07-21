2f0e5a81fb28f25530e22df0faa010dd2551a663
MarvinLvn <marvin.lavechin@ensimag.grenoble-inp.fr>
2019-07-21 19:38:21 -0400

    Modify scripts to use fair/ scripts

--- fair/run_gridsearch.sh
+++ fair/run_gridsearch.sh
@@ -4,6 +4,6 @@ cd ..
 for dir in gridsearch_experiments/*; do
     protocol=$(basename $dir)
     protocol=${protocol%.*}
-    sbatch train.sh $dir $protocol
-    sbatch validate_der.sh SPEECH $dir $protocol
+    sbatch fair/train.sh $dir $protocol
+    sbatch fair/validate_der.sh SPEECH $dir $protocol
 done
